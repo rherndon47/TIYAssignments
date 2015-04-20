8e8eeb99f1735f56d6b7432f0d2ec18b219738b5
Richard Herndon <richardherndon1@Richards-MacBook-Pro.local>
2015-04-20 10:40:01 -0400

    Add updates for OGC

--- OGC Folder/OGCSample/OGCSample/ConcertSecondViewController.h
+++ OGC Folder/OGCSample/OGCSample/ConcertSecondViewController.h
@@ -10,6 +10,5 @@
 
 @interface ConcertSecondViewController : UIViewController
 
-
 @end
 
--- OGC Folder/OGCSample/OGCSample/ConcertSecondViewController.m
+++ OGC Folder/OGCSample/OGCSample/ConcertSecondViewController.m
@@ -8,7 +8,12 @@
 
 #import "ConcertSecondViewController.h"
 
-@interface ConcertSecondViewController ()
+@interface ConcertSecondViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIImageView *imageView;
+@property (strong, nonatomic) UIImage *image;
+@property (strong, nonatomic) NSURL *imageURL;
 
 - (IBAction)showMapPlazaLive:(UIButton *)sender;
 
@@ -18,6 +23,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self.scrollView addSubview:self.imageView];
+    self.imageURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.orlandogaychorus.org/images/One-Voice-poster-format--345.jpg"]];
     // Do any additional setup after loading the view, typically from a nib.
 
 }
@@ -27,9 +35,53 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - IBAction Methods
+
 - (IBAction)showMapPlazaLive:(UIButton *)sender
 {
 
+}
+
+#pragma mark - UIScrollView methods
+
+- (UIImageView *)imageView
+{
+    if (!_imageView)
+    {
+        _imageView = [[UIImageView alloc] init];
+    }
+    return _imageView;
+}
+
+- (UIImage *)image
+{
+    return self.imageView.image;
+}
+
+- (void)setImage:(UIImage *)image
+{
+    self.imageView.image = image;
+    [self.imageView sizeToFit];
+}
+
+- (void)setImageURL:(NSURL *)imageURL
+{
+    _imageURL = imageURL;
+    self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:self.imageURL]];
+    self.scrollView.contentSize = self.image ? self.image.size :CGSizeZero;
+}
+
+- (void)setScrollView:(UIScrollView *)scrollView
+{
+    _scrollView = scrollView;
+    _scrollView.minimumZoomScale = 0.2;
+    _scrollView.maximumZoomScale = 2.0;
+    _scrollView.delegate = self;
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.imageView;
 }
 
 
